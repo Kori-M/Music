b445ddbaff0b0f0be0afc6134990a7fa1d81c7fe
Kori-M <karinamedvick@gmail.com>
2026-06-08 15:15:51 -0400

    Add files via upload

---
+++ songs.js
@@ -0,0 +1,77 @@
+const songs = [
+    {
+        title: "Ordinary",
+        artist: "Alex Warren",
+        searchTerm: "ordinary alex warren"
+    },
+    {
+        title: "PASSENGER",
+        artist: "Alex Warren",
+        searchTerm: "passenger alex warren"
+    },
+    {
+        title: "Getaway Car",
+        artist: "Alex Warren",
+        searchTerm: "getaway car alex warren"
+    },
+    {
+        title: "Never Be Far",
+        artist: "Alex Warren",
+        searchTerm: "never be far alex warren"
+    },
+    {
+        title: "First Time On Earth",
+        artist: "Alex Warren",
+        searchTerm: "first time on earth alex warren"
+    },
+    {
+        title: "You'll Be Alright, Kid",
+        artist: "Alex Warren",
+        searchTerm: "youll be alright kid alex warren"
+    },
+    {
+        title: "Save You a Seat",
+        artist: "Alex Warren",
+        searchTerm: "save you a seat alex warren"
+    },
+    {
+        title: "Troubled Waters",
+        artist: "Alex Warren",
+        searchTerm: "troubled waters alex warren"
+    },
+    {
+        title: "You Can't Stop This",
+        artist: "Alex Warren",
+        searchTerm: "you cant stop this alex warren"
+    },
+    {
+        title: "Bloodline",
+        artist: "Alex Warren, Jelly Roll",
+        searchTerm: "bloodline alex warren"
+    },
+    {
+        title: "Carry You Home",
+        artist: "Alex Warren",
+        searchTerm: "carry you home alex warren"
+    },
+    {
+        title: "The Outside",
+        artist: "Alex Warren",
+        searchTerm: "the outside alex warren"
+    },
+    {
+        title: "Heaven Without You",
+        artist: "Alex Warren",
+        searchTerm: "heaven without you alex warren"
+    },
+    {
+        title: "Everything",
+        artist: "Alex Warren",
+        searchTerm: "everything alex warren"
+    },
+    {
+        title: "Burning Down",
+        artist: "Alex Warren",
+        searchTerm: "burning down alex warren"
+    },
+];
